5e4e3a6ee8fbf4c2353aaf97c5c47bf035bd5c72
aDerringer <47407127+aDerringer@users.noreply.github.com>
2020-01-11 18:38:15 -0800

    Remove excess files

--- mdf_reader.py
+++
@@ -1,7 +0,0 @@
-from asammdf import MDF, Signal
-
-mdf = MDF("./2018-07-01_15-58-05Ehingen.mf4")
-
-for sig in mdf.iter_channels():
-    print('Sig repr', sig)
-    print('Samples and timestamps:', sig.samples, sig.timestamps)
