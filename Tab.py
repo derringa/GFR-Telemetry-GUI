8a5eeb7c09a8eb6af4cefa7d45cbcfd93a60faf8
aDerringer <47407127+aDerringer@users.noreply.github.com>
2020-01-17 17:30:42 -0800

    Commenting improved

--- Tab.py
+++ Tab.py
@@ -1,38 +1,29 @@
+# Author:           Andrew Derringer
+# Last Modified:    1/17/2020
+# Description:      Tab class is build for pyqt5 to generate a tab object that is passed necessary information
+#                   to plot graph without standard behavior of creating another application window.
+
 import itertools
 import pyqtgraph
-import time
-from PyQt5 import QtWidgets, QtCore, QtGui
-#from QtWidgets import QApplication, QMainWindow, QWidget, QGroupBox, QSlider, QLabel, QVBoxLayout, QHBoxLayout, \
-    #QPushButton, QFormLayout, QGridLayout, QTabWidget
+from PyQt5 import QtWidgets
 
 
 class Tab:
 
-    #id_iter = itertools.count()
-
     def __init__(self, sensor_name, x_vals, y_vals):
-        #self.id = next(Tab.id_iter)
-        self.title = sensor_name
+        # Tab data variables.
+        self.title = sensor_name # Saved to apply as widget title in pyqt application.
+        self.x = x_vals # Applied to x-axis of pyqtplot.
+        self.y = y_vals # Applied to y-axis of pyqtplot.
+
+        # Declare pyqt widgets needed to build graphing tab.
         self.tab = QtWidgets.QWidget()
-
         self.graph = pyqtgraph.PlotWidget()
-        #self.timeStamp = "00:00:00"
-        self.x = x_vals
-        self.y = y_vals
+        self.layout = QtWidgets.QVBoxLayout()
 
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.graph)
+        # Combine pyqt objects.
+        self.layout.addWidget(self.graph)
+        self.tab.setLayout(self.layout)
+
+        # Plot graph within the tab.
         self.graph.plot(self.x, self.y)
-        self.tab.setLayout(layout)
-
-    # def updateData(self, newX, newY):
-    #     self.x.extend(newX)
-    #     self.y.extend(newY)
-    #     self.x = newX
-    #     self.y = newY
-    #     self.graph.plot(self.x, self.y)
-
-    # def updateTimeStamp(self, newTime):
-    #     self.timeStamp = newTime
-
-
--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
+# Author:           Andrew Derringer
+# Last Modified:    1/17/2020
+# Description:      PyQt5 application that generates customizable display of CAN data using MDF and DBC files.
+#                   This application was made using data and specifications from Oregon State Univesity Global
+#                   formula racing team.
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 from asammdf import MDF
-#from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGroupBox, QSlider, QLabel, QVBoxLayout, QHBoxLayout, \
-    #QPushButton, QFormLayout, QGridLayout, QTabWidget, QMessageBox, QInputDialog, QLineEdit, QLabel
-#from PyQt5.QtCore import Qt, QSize
-#from PyQt5.QtGui import QPainter, QBrush, QPen, QFont, QColor
-from random import random
 import numpy as np
 from numpy import sin, cos, pi
 from enum import Enum
@@ -57,30 +58,47 @@ class main(QtWidgets.QMainWindow):
         self.channel_list = []
         self.mdf_list = []
 
-        # Apply title and staring size and location
-        # for window on startup
+        # Apply startup characteristics for window
         self.setWindowTitle("GFR Telemtry Data")
         self.setGeometry(0, 0, 1250, 750)
 
-        # centralWidget houses layout features
-        # and smaller widgets within the app
+        # centralWidget houses layout features within the app
         self.centralWidget = QtWidgets.QWidget(self)
         self.setCentralWidget(self.centralWidget)
 
-        ################################################################
-        # Static tool bar
-        ################################################################
+        # Section for adding toolbar sections and subsections
+        self.build_toolbar()
 
-        # Tool bar setup
-        self.statusBar()
-        mainMenu = self.menuBar()
+        # Sections for placing all layouts and widgets
+        self.place_widgets()
+
+        # holds instance up add tab popup so that it data
+        # is not cleaned up immediately upon close
+        self.popup = None
+
+
+    # def tab_window(self):
+    #     self.popup = popupWindow()
+    #     self.popup.main = self
+    #     self.popup.show()
+
+
+    # def add_tab(self, item):
+    #     #item.updateData(self.hour, self.temperature)
+    #     self.tabs.addTab(item.tab, item.title)
+
+
+    def build_toolbar(self):
+        # Toolbar setup
+        self.statusBar() # Along bottom of screen
+        mainMenu = self.menuBar() # Along top of screen
 
         # File drop down
         fileMenu = mainMenu.addMenu("&File")
 
         # File drop down option - exit
         extractAction = QtGui.QAction("&Exit", self)
-        extractAction.setShortcut("Ctrl+Q")
+        extractAction.setShortcut("Ctrl+Q") # Set key-board shortcut
         extractAction.setStatusTip("&Leave the App")
         extractAction.triggered.connect(self.close_application)
         fileMenu.addAction(extractAction)  # add to file drop down
@@ -95,10 +113,8 @@ class main(QtWidgets.QMainWindow):
         # addTab.triggered.connect(self.tab_window)
         # viewMenu.addAction(addTab) # add to view drop down
 
-        ################################################################
-        # Main Widget Layout
-        ################################################################
 
+    def place_widgets(self):
         # Create horizontal format for main widget subsections
         self.horizontalSections = QtWidgets.QHBoxLayout()
         self.centralWidget.setLayout(self.horizontalSections)
@@ -130,79 +146,83 @@ class main(QtWidgets.QMainWindow):
         self.plot_channels.clicked.connect(self.load_plots) # On click call member function load_plots
         self.left_vert_sec.addWidget(self.plot_channels)
 
-        # Right subsection - Tabbed graphic data
-        # self.tabList = []
+        # Middle subsection - Tabbed graphic data
         self.tabs = QtWidgets.QTabWidget()
         self.horizontalSections.addWidget(self.tabs)
 
-        # Add horizontal subsections to central widget
-
-
-        # holds instance up add tab popup so that it data
-        # is not cleaned up immediately upon close
-        self.popup = None
-
-    # def tab_window(self):
-    #     self.popup = popupWindow()
-    #     self.popup.main = self
-    #     self.popup.show()
-
-    # def add_tab(self, item):
-    #     #item.updateData(self.hour, self.temperature)
-    #     self.tabs.addTab(item.tab, item.title)
 
     def close_application(self):
         print("Good-Bye!")
         sys.exit()
 
+
     def get_mf4_file(self):
-        mf4_path = QtWidgets.QFileDialog.getOpenFileName(self, 'Select file', 'c:\\', "MDF (*.mf4)")
-        mdf_file = MDF(mf4_path[0])
-        self.mdf_extracted = mdf_file.extract_can_logging([self.dbc_path[0]])
+        # Get user selected MDF file path and generate an mdf object using asammdf library.
+        mf4_path = QtWidgets.QFileDialog.getOpenFileName(self, 'Select file', 'c:\\', "MDF (*.mf4)") # get file path.
+        mdf_file = MDF(mf4_path[0])  # generate mdf object using path string.
+        self.mdf_extracted = mdf_file.extract_can_logging([self.dbc_path[0]]) # extract data with dbc file path string.
+
+        # Collect name of all channels into a list.
         for ch in self.mdf_extracted:
             self.mdf_list.append(ch.name)
+
+        # Compare list of DBC channels to those in MDF file and remove those not in both.
+        for i in reversed(range(self.channel_selectors.count())):
+            ch_name = self.channel_selectors.item(i).text()
+            if ch_name not in self.mdf_list: 
+                self.channel_selectors.takeItem(i) # Not present then delete from checkbox list.
+                if ch_name in self.channel_list:
+                    self.channel_list.remove(ch_name) # If already checked then also delete from selected channels list.
+
+        # Activate the Plot Channel button only after MDF file is loaded and operated on.
         self.plot_channels.setEnabled(True)
 
 
     def get_dbc_file(self):
+        # Get user slected DBC file path.
         self.dbc_path = QtWidgets.QFileDialog.getOpenFileName(self, 'Select file', 'c:\\', "DBC (*.dbc)")
+
+        # Open file and add channel name to list under conditions.
         with open(self.dbc_path[0], 'r') as fd:
             for line in fd:
-                line_list = line.split(None, 2)
-                if not line_list:
+                line_list = line.split(None, 2) # grab first 2 space delimited strings.
+                if not line_list: # disregard empty lines.
                     continue
-                elif line_list[0] == 'SG_':
-                    item = QtWidgets.QListWidgetItem(line_list[1])
+                elif line_list[0] == 'SG_': # only work with lines whose first string is 'SG_'.
+                    item = QtWidgets.QListWidgetItem(line_list[1]) # add second string to a checkbock widget list item.
                     item.setCheckState(False)
-                    #print(item.text()) # for testing
                     self.channel_selectors.addItem(item)
+
+        # Activate the Load MF4 File button only after DBC file is loaded and operated on.
         self.upload_mf4_file.setEnabled(True)
     
+
     def manage_channels(self, item):
+        # If changed checkbox is unchecked then remove from the desired channels list but if checked then add to list.
         if item.checkState() == False:
             self.channel_list[:] = [ch for ch in self.channel_list if ch != item.text()]
-            print("Unchecked!")
         else:
             self.channel_list.append(item.text())
-            print("Checked!")
-        print(self.channel_list)
+
+
 
     def load_plots(self):
-        curr_tabs = []
+        curr_tabs = [] # Temporary list of tabs already displayed to avoid reproducing on refresh.
+
+        # Iterate through tabs and if no longer on desired channel list them remove them else add to current tabs list.
         for i in reversed(range(self.tabs.count())):
             tab_name = self.tabs.tabText(i)
-            if tab_name not in self.channel_list:
-                self.tabs.removeTab(i)
+            if tab_name not in self.channel_list: 
+                self.tabs.removeTab(i) # Not present them delete.
             else:
-                curr_tabs.append(tab_name)
+                curr_tabs.append(tab_name) # Present then add to current tabs temp list.
 
+        # If channel doesn't have a tab then create tab with graph.
         for ch in self.channel_list:
-            #print(ch)
-            if ch in self.mdf_list and ch not in curr_tabs:
-                obj_data = self.mdf_extracted.get(ch)
-                new_tab = Tab(ch, obj_data.timestamps, obj_data.samples)
-                self.tabs.addTab(new_tab.tab, new_tab.title)
-
+            if ch not in curr_tabs:
+                obj_data = self.mdf_extracted.get(ch) # Get channel by specific name.
+                new_tab = Tab(ch, obj_data.timestamps, obj_data.samples) # Generate tab object using mdf channel name and time and data values.
+                self.tabs.addTab(new_tab.tab, new_tab.title) # Add tab to widget.
 
 
 if __name__ == "__main__":
